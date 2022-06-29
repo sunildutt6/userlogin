d852677000b1e9d065a00c0c4e88b15210af3420
sunil <sunildutt6@hotmail.com>
2022-06-29 20:03:28 +0200

    initial commit

---
+++ src/main/java/com/DashboardServlet.java
@@ -0,0 +1,62 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class DashboardServlet
+ */
+@WebServlet("/dashboard")
+public class DashboardServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public DashboardServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+
+		HttpSession session = request.getSession();
+		String userid = (String) session.getAttribute("userid");
+		String password = (String) session.getAttribute("password");
+
+		if (userid != null && password != null) {
+			out.println("Hello " + userid);
+			out.println("<a href=logout>Logout </a>");
+		} else {
+			out.println("Please login first");
+		}
+
+		out.println("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
